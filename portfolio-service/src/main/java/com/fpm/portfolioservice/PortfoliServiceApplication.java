7fc7e58c5357a6d8435774ff62039923cea8d37c
ajay.k.kumar <a.kumar@list-group.com>
2024-07-19 17:08:43 +0530

    Add in user files

---
+++ portfolio-service/src/main/java/com/fpm/portfolioservice/PortfoliServiceApplication.java
@@ -0,0 +1,11 @@
+package com.fpm.portfolioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoliServiceApplication.class, args);
+    }
+}
