51008d444a0a7507f76fac5359c2ea29e748c94e
Ajay Kumar <ajayjnv02@gmail.com>
2025-05-01 22:29:22 +0530

    change in startegy to develop

---
+++ graphql-gateway/src/main/java/com/fpm/trade/service/TradeServiceApp.java
@@ -0,0 +1,7 @@
+package com.fpm.trade.service;
+
+public class TradeServiceApp {
+    public static void main(String[] args) {
+       
+    }
+}
--- user-service/src/main/java/com/fpm/userservice/model/User.java
+++
@@ -1,8 +0,0 @@
-package com.fpm.userservice.model;
-
-public record User(
-        Long id,
-        String username,
-        String email,
-        String passwordHash
-) {}
